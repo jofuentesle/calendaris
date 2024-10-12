52ebacc99a36ccf550d34960192357e031d3f39f
jofuentesl <jofuentesle@uoc.edu>
2024-10-12 10:57:45 +0200

    last update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
-
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
