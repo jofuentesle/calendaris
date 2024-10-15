1cf52307b5710bbdbf4ab1019a6a2063cadd0135
jofuentesl <jofuentesle@uoc.edu>
2024-10-15 12:30:05 +0200

    update form no works

--- src/pages/api/send-quotes.ts
+++ src/pages/api/send-quotes.ts
@@ -1,38 +1,44 @@
-// src/pages/api/send-email.ts
 import type { APIRoute } from 'astro';
 import sendgrid from '@sendgrid/mail';
 
 // Configura la API Key de SendGrid
 sendgrid.setApiKey(import.meta.env.SENDGRID_API_KEY);
 
-// Función para manejar la solicitud
 export const post: APIRoute = async ({ request }) => {
-  console.log('Solicitud recibida en /api/send-quotes');
-  console.log(sendgrid);
   try {
-    // Extraer datos del formulario
     const formData = await request.formData();
-    const to = formData.get('to') as string;
-    const subject = formData.get('subject') as string;
-    const message = formData.get('message') as string;
+    
+    const nombre = formData.get('nombre') as string;
+    const email = formData.get('email') as string;
+    const telefono = formData.get('telefono') as string;
+    const empresa = formData.get('empresa') as string;
+    const tipoCalendario = formData.get('tipoCalendario') as string;
+    const tamaño = formData.get('tamaño') as string;
+    const cantidad = formData.get('cantidad') as string;
 
-    // Verificar que los datos necesarios estén presentes
-    if (!to || !subject || !message) {
+    if (!nombre || !email || !tipoCalendario || !tamaño || !cantidad) {
       return new Response('Faltan campos requeridos', { status: 400 });
     }
+    console.log(formData);
+    const message = `
+      Nombre: ${nombre}
+      Email: ${email}
+      Teléfono: ${telefono}
+      Empresa: ${empresa}
+      Tipo de Calendario: ${tipoCalendario}
+      Tamaño: ${tamaño}
+      Cantidad: ${cantidad}
+    `;
 
-    // Configurar el correo electrónico
     const msg = {
-      to,
-      from: 'noreply@reprodisseny.com', // Reemplaza con tu dirección de correo
-      subject,
+      to: 'ventas@reprodisseny.com',
+      from: 'noreply@reprodisseny.com',
+      subject: `Solicitud de oferta de calendario - ${tipoCalendario}`,
       text: message,
     };
 
-    // Enviar el correo electrónico con SendGrid
     await sendgrid.send(msg);
 
-    // Respuesta exitosa
     return new Response('Correo enviado correctamente', { status: 200 });
   } catch (error) {
     console.error('Error al enviar el correo:', error);
